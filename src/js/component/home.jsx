1d6c1caab2b0c1de6a213e3a297be56b08195813
jaranedag <jaranedag@udd.cl>
2022-11-26 16:29:36 +0000

    tarea todo list fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState ("");
+	const [tareas,setTareas] = useState([]);
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag")
+  .then(response => response.json())
+  .then(result => setTareas(result))
+  .catch(error => console.log('error', error));
+						
+	},[])
+     function actualizar (){
+		var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify(tareas);
+
+var requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+	 }
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	       <div className="container">
+			 <h1>Mi lista de Tareas </h1>
+                 <ul>
+					<li><input type="text"
+                      onChange={(e)=> setInputValue(e.target.value)}
+					  value={inputValue}
+					  onKeyPress={(e) => { 
+						 if(e.key  === "Enter") {
+							 setTareas(tareas.concat({label: inputValue, done: false}));
+							 setInputValue("")
+							 actualizar();
+							  }
+						  }}
+					 placeholder="Que necesitas hacer?" /></li>
+
+					
+					 
+					 {tareas.map((t,i)=> ( 
+						 <li key={i}> 
+							{t.label}<i  className="fas fa-times text-danger  d-flex justify-content-end" onClick={()=> {setTareas(tareas.filter((t,index)=>i != index))
+									actualizar();
+									}}
+								></i>
+						 </li>
+					  ))}
+				
+				</ul>
+				<div className="footer text-center ">{tareas.length} Tareas</div>
+		   </div>
 	);
 };
 
 export default Home;
+
