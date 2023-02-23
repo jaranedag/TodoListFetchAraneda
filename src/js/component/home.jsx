e1d757a7a3c5c5768fc622b3add36fb5a6490f43
jaranedag <jaranedag@udd.cl>
2023-02-23 19:54:17 +0000

    actualizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,93 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
+
+const Todo = () =>{
+	const [tareas,setTarea] = useState ([]);
+	const borrarTarea = (i) => {
+		setTarea(
+			tareas.filter((value,index)=>{
+				return index !== i;
+			})
+		);
+	};
+
+
+function putApi(){
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	var raw = JSON.stringify(tareas);
+
+	var requestOptions = {
+		method: "PUT",
+		headers: myHeaders,
+		body: raw,
+		redirect: "follow",
+	};
+	
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag",requestOptions)
+		.then((response) => response.json())
+		.then((result) => console.log(result))
+		.catch((error) => console.log("error", error));
+	
+}
+useEffect(() => {
+	var requestOptions = {
+		method: "GET",
+		redirect:"follow"
+	};
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag",requestOptions)
+		.then((respuesta) => respuesta.json())
+		.then((data) => setTarea(data))
+		.catch(error => console.log("error",error));
+}, []);
+useEffect(()=>{
+	putApi();
+},[tareas])
+
+return (
+	<div className="container">
+	  <h1>Mi lista de Tareas </h1>
+		  <ul>
+			 <li><input type="text"
+			   
+			   onKeyPress={(event) => {
+				if (event.key == "Enter") {
+					setTarea([...tareas, { label: event.target.value, done: false }]);
+					event.target.value = "";
+				}
+			}}
+			  placeholder="Que necesitas hacer?" /></li>
+
+			
+			
+		 
+		 </ul>
+		 {tareas?.map((value,index) =>{
+				return (
+				<li key={index}>
+					{value.label}<i className="fas fa-times text-danger  d-flex justify-content-end" onClick={() => borrarTarea(index)}></i>
+				</li>
+			)}
+			)}
+		 <div className="footer text-center ">{tareas.length} Tareas</div>
+	</div>
+);
+
+};
+export default Todo;
 //create your first component
-const Home = () => {
+/*const Home = () => {
 	const [inputValue, setInputValue] = useState ("");
 	const [tareas,setTareas] = useState([]);
+	
+
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag")
 			.then(response => response.json())
@@ -37,6 +118,13 @@ fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag", requestOptio
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 	 }
+	 function eliminarTarea(index) {
+		setTareasEliminadas(tareasEliminadas.concat(tareas[index]));
+		setTareas(tareas.filter((t, i) => i !== index));
+		console.log(tareas);
+		actualizar()
+	  }
+
 	return (
 	       <div className="container">
 			 <h1>Mi lista de Tareas </h1>
@@ -48,7 +136,7 @@ fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag", requestOptio
 						 if(e.key  === "Enter") {
 							 setTareas(tareas.concat({label: inputValue, done: false}));
 							 setInputValue("")
-							 actualizar();
+							 actualizar()
 							  }
 						  }}
 					 placeholder="Que necesitas hacer?" /></li>
@@ -58,7 +146,7 @@ fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag", requestOptio
 					 {tareas.map((t,i)=> ( 
 						 <li key={i}> 
 							{t.label}<i  className="fas fa-times text-danger  d-flex justify-content-end" onClick={()=> {setTareas(tareas.filter((t,index)=>i != index))
-									actualizar();
+									
 									}}
 								></i>
 						 </li>
@@ -71,5 +159,4 @@ fetch("https://assets.breatheco.de/apis/fake/todos/user/jaranedag", requestOptio
 	
 };
 
-export default Home;
-
+export default Home;*/
